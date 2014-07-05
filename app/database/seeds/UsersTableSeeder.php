9be5ca6e4376538aec42d23787a51ebc7dba68c9
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-07-05 10:40:08 +0200

    initial commit

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        $dt      = Carbon::now();
+        $dateNow = $dt->toDateTimeString();
+
+        $users = array(
+            array(
+                'username'          => 'admin',
+                'email'             => 'admin@example.org',
+                'password'          => Hash::make('admin'),
+                'confirmed'         => 1,
+                'confirmation_code' => md5(microtime() . Config::get('app.key')),
+                'created_at'        => $dateNow,
+                'updated_at'        => $dateNow,
+                'remember_token'    => 'azerty'
+            ),
+            array(
+                'username'          => 'user',
+                'email'             => 'user@example.org',
+                'password'          => Hash::make('user'),
+                'confirmed'         => 1,
+                'confirmation_code' => md5(microtime() . Config::get('app.key')),
+                'created_at'        => $dateNow,
+                'updated_at'        => $dateNow,
+                'remember_token'    => 'azerty'
+
+            )
+        );
+
+        DB::table('users')->insert($users);
+    }
+
+}
---
+++ app/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::group(array('prefix' => 'api/v1'), function() {
+
+	Route::get('auth', 'Tappleby\AuthToken\AuthTokenController@index');
+	Route::post('auth', 'Tappleby\AuthToken\AuthTokenController@store');
+	Route::delete('auth', 'Tappleby\AuthToken\AuthTokenController@destroy');
+
+	Route::group(array('before' => 'auth.token'), function() {
+		Route::get('/', function() {
+			return "Protected resource";
+		});
+	});
+
+});
