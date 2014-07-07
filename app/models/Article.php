7431d037e6f152eeec16b3abd117285e1ca05d50
alahaxe <alahaxe@vm.(none)>
2014-07-07 18:03:31 +0200

    Ardent for article and remove presenter

--- app/models/Article.php
+++ app/models/Article.php
@@ -1,6 +1,17 @@
 <?php
 
-class Article extends \Eloquent {
+use \LaravelBook\Ardent\Ardent;
+
+class Article extends Ardent {
+
+    public static $rules = array(
+        'title'     => 'required|min:5',
+        'url'       => 'required|active_url',
+        'slug'      => 'required|unique:articles,slug',
+        'indexable' => 'required',
+        'body'      => 'required|min:5',
+    );
+
 	protected $fillable = [];
 
 	 /**
