72aa09aeb12904a988b63acd8ad24b58b0016e32
LAHAXE Arnaud <Arnaud.LAHAXE.ext@boursorama.fr>
2014-07-08 14:38:37 +0200

    fix log namespace

--- app/observers/ArticleObserver.php
+++ app/observers/ArticleObserver.php
@@ -7,22 +7,22 @@ class ArticleObserver extends Observer
 {
     public function saved(Model $model)
     {
-        Log::info("saved " . $model->id);
+        \Log::info("saved " . $model->id);
     }
 
     public function updated(Model $model)
     {
-        Log::info("updated " . $model->id);
+        \Log::info("updated " . $model->id);
     }
 
     public function deleted(Model $model)
     {
-        Log::info("deleted " . $model->id);
+        \Log::info("deleted " . $model->id);
     }
 
     public function restored(Model $model)
     {
-        Log::info("restored " . $model->id);
+        \Log::info("restored " . $model->id);
     }
 
 }
