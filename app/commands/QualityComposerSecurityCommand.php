c05ba33b9c055f13c790be2f09c26264d91ba2cc
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-07-20 13:12:02 +0200

    integrate last version of sensiolab/security-checker

--- app/commands/QualityComposerSecurityCommand.php
+++ app/commands/QualityComposerSecurityCommand.php
@@ -38,11 +38,52 @@ class QualityComposerSecurityCommand extends Command
     {
         $checker = new SecurityChecker();
         $alerts = $checker->check('composer.lock');
-        if($checker->getLastVulnerabilityCount() === 0) {
-            $this->info($alerts);
-        }else{
-            $this->error($alerts);
+
+        $this->displayResults($this->getOutput(), 'composer.log', $alerts);
+
+    }
+
+    private function displayResults(\Symfony\Component\Console\Output\OutputInterface $output, $lockFilePath, array $vulnerabilities)
+    {
+        $output->writeln("\n<fg=blue>Security Check Report\n~~~~~~~~~~~~~~~~~~~~~</>\n");
+        $output->writeln(sprintf('Checked file: <comment>%s</>', realpath($lockFilePath)));
+        $output->write("\n");
+
+        if ($count = count($vulnerabilities)) {
+            $status = 'CRITICAL';
+            $style = 'error';
+        } else {
+            $status = 'OK';
+            $style = 'bg=green;fg=white';
         }
+
+        $output->writeln($this->getHelper('formatter')->formatBlock(array('['.$status.']', $count.' packages have known vulnerabilities'), $style, true));
+        $output->write("\n");
+
+        if (0 !== $count) {
+            foreach ($vulnerabilities as $dependency => $issues) {
+                $dependencyFullName = $dependency.' ('.$issues['version'].')';
+                $output->writeln('<info>'.$dependencyFullName."\n".str_repeat('-', strlen($dependencyFullName))."</>\n");
+
+                foreach ($issues['advisories'] as $issue => $details) {
+                    $output->write(' * ');
+                    if ($details['cve']) {
+                        $output->write('<comment>'.$details['cve'].': </comment>');
+                    }
+                    $output->writeln($details['title']);
+
+                    if ('' !== $details['link']) {
+                        $output->writeln('   '.$details['link']);
+                    }
+
+                    $output->writeln('');
+                }
+            }
+        }
+
+        $output->writeln("<bg=yellow;fg=white>            </> This checker can only detect vulnerabilities that are referenced");
+        $output->writeln("<bg=yellow;fg=white> Disclaimer </> in the SensioLabs security advisories database. Execute this");
+        $output->writeln("<bg=yellow;fg=white>            </> command regularly to check the newly discovered vulnerabilities.\n");
     }
 
     /**
