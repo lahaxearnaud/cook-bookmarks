89c1eea474fdf25d45b10931668b2c3b49fff03f
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-07-25 07:50:50 +0200

    Add pseudo test on observer

---
+++ tests/unit/contract/ModelObserverCase.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arnaud
+ * Date: 25/07/14
+ * Time: 07:15
+ */
+
+abstract class ModelObserverCase extends TestCase {
+
+    /**
+     * @var \Observers\Models\ObserverInterface
+     */
+    protected $observer;
+
+    /**
+     * @return mixed
+     */
+    public function getObserver ()
+    {
+        return $this->observer;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->observer = $this->buildObserver();
+    }
+
+    abstract function getDatabaseModel();
+
+    abstract function getNonSavedModel();
+
+
+    abstract protected function buildObserver();
+
+
+    abstract public function testSavingOk();
+
+    abstract public function testSavedOk();
+
+    abstract public function testUpdatingOk();
+
+    abstract public function testUpdatedOk();
+
+    abstract public function testCreatingOk();
+
+    abstract public function testCreatedOk();
+
+    abstract public function testDeletingOk();
+
+    abstract public function testDeletedOk();
+
+    abstract public function testRestoringOk();
+
+    abstract public function testRestoredOk();
+}
