b96c6c1048bf1b90fe173d1f3bdffc78e9e87525
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-08-04 13:44:36 +0200

    No SSL in article extract url

--- tests/api/articles/ArticlesExtractFromUrlCept.php
+++ tests/api/articles/ArticlesExtractFromUrlCept.php
@@ -9,7 +9,7 @@
 $I = new Ninja($scenario);
 $I->wantTo('Extract article from an url');
 $I->call('articles/extractFromUrl', 'POST', array(
-    'url'   => 'https://github.com/laravel/laravel',
+    'url'   => 'http://laravel.com/docs/quick',
     'markdown' => false,
 ), 200);
 $I->validateResponseWithClosure(function (Codeception\Module\NinjaHelper $api, $response) {
@@ -25,7 +25,7 @@ $I->validateResponseWithClosure(function (Codeception\Module\NinjaHelper $api, $
 
 $I->wantTo('Extract article from an url in markdown');
 $I->call('articles/extractFromUrl', 'POST', array(
-    'url'   => 'https://github.com/laravel/laravel',
+    'url'   => 'http://laravel.com/docs/quick',
     'markdown' => true,
 ), 200);
 $I->validateResponseWithClosure(function (Codeception\Module\NinjaHelper $api, $response) {
@@ -33,7 +33,7 @@ $I->validateResponseWithClosure(function (Codeception\Module\NinjaHelper $api, $
     $api->isEquals('success', $response['success'], TRUE);
 
     $api->isType('title', 'STRING', $response['title']);
-    $api->isEquals('title', $response['title'], 'laravel/laravel · GitHub');
+    $api->isEquals('title', $response['title'], 'Laravel - The PHP Framework For Web Artisans.');
 
     $api->isType('body', 'STRING', $response['body']);
 });
@@ -49,7 +49,7 @@ $I->validateResponseWithClosure(function (Codeception\Module\NinjaHelper $api, $
 
 $I->wantTo('Extract article from empty url');
 $I->call('articles/extractFromUrl', 'POST', array(
-    'url'   => 'https://github.com/DUMMY/DUMMY'
+    'url'   => 'http://dummy.laravel.com'
 ), 400);
 $I->validateResponseWithClosure(function (Codeception\Module\NinjaHelper $api, $response) {
     $api->isType('success', 'BOOLEAN', $response['success']);
