bc0f453488447e17fb1e3a25b5429818abb56def
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-08-06 11:13:03 +0200

    remove useless migrate

--- tests/api/_bootstrap.php
+++ tests/api/_bootstrap.php
@@ -7,12 +7,9 @@ $app->setRequestForConsoleEnvironment();
 $app->boot();
 
 if(!defined('setup')) {
-    Artisan::call('migrate:reset');
-    Artisan::call('migrate');
     Artisan::call('db:seed');
     Artisan::call('es:uninstall');
     Artisan::call('es:install');
-Log::info('##############');
     define('setup', TRUE);
 }
 
