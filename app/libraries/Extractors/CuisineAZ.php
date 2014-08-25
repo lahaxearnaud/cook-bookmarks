d00e8a4c039f763cd36d7e19e133de68749385ab
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-08-25 08:51:21 +0200

    article extractor

--- app/libraries/Extractors/CuisineAZ.php
+++ app/libraries/Extractors/CuisineAZ.php
@@ -5,7 +5,17 @@ namespace Extractors;
 class CuisineAZ extends ArticleExtractor{
 
     public function extract($html, $url = '') {
+		$html = str_get_html($html);
 
+    	$title = $html->find('.recetteH1', 0);
+    	$ingredients = $html->find('#ingredients');
+    	$preparations = $html->find('#preparation');
+
+    	return array(
+            'title' => (is_null($title)?'':$title->plaintext,
+            'body' => ((is_null($ingredients)?'':$ingredients->outertext) .'<br/>' . (is_null($preparations)?'':$preparations->outertext),
+            'success' => true
+        );
     }
 
 };
--- app/libraries/Extractors/Marmiton.php
+++ app/libraries/Extractors/Marmiton.php
@@ -2,10 +2,22 @@
 
 namespace Extractors;
 
+require public_path().'/../vendor/simplehtmldom/simplehtmldom/simple_html_dom.php';
+
+
 class Marmiton extends ArticleExtractor{
 
     public function extract($html, $url = '') {
+    	$html = str_get_html($html);
 
+    	$title = $html->find('title', 0);
+    	$body = $html->find('.m_content_recette_main', 0);
+
+    	return array(
+            'title' => (is_null($title)?'':$title->plaintext,
+            'body' => (is_null($body)?'':$body->innertext,
+            'success' => true
+        );
     }
 
 };
