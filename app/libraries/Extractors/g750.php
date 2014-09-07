a1e91a2f9d4762b124112c3dee28873c680195a5
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-09-07 12:54:48 +0200

    remove unwanted title of step in g750 extractor

--- app/libraries/Extractors/g750.php
+++ app/libraries/Extractors/g750.php
@@ -2,31 +2,21 @@
 
 namespace Extractors;
 
-class g750 extends AbstractExtractor
-{
+class g750 extends AbstractExtractor {
 
-    public function getTitleCssSelector()
-    {
-        return 'h1';
-    }
+	public function getTitleCssSelector() {
+		return 'h1';
+	}
 
-    public function getYieldCssSelector()
-    {
-        return 'div.main > div.row > div.hrecipe > div p .yield';
-    }
+	public function getYieldCssSelector() {
+		return 'div.main > div.row > div.hrecipe > div p .yield';
+	}
 
-    public function getIngredientsCssSelector()
-    {
-        return 'div.main > div.row > div.hrecipe > div.row > ul > li';
-    }
+	public function getIngredientsCssSelector() {
+		return 'div.main > div.row > div.hrecipe > div.row > ul > li';
+	}
 
-    public function getPreparationsCssSelector()
-    {
-        return '.instructions';
-    }
-
-    public function getPreparations($domHtml)
-    {
-        return $domHtml->find($this->getPreparationsCssSelector(), 0)->innertext;
-    }
+	public function getPreparationsCssSelector() {
+		return '.instructions .recette_etape_texte';
+	}
 }
