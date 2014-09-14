d5179baea12b284acc7ab86d427baf6e8a8439d7
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-09-14 12:49:10 +0200

    Cuisine notre famille extractor

--- app/config/extractor.php
+++ app/config/extractor.php
@@ -2,23 +2,24 @@
 
 return array(
 
-    'stopword' => array('blank', 'header', 'social', 'facebook', 'twitter', 'logo', '1px', 'star', 'rating', 'email', 'footer', 'ytimg', 'outil', 'tool', 'ad.', 'adserv', 'pinterest', 'google', 'encart', 'reddit'),
+	'stopword' => array('blank', 'header', 'social', 'facebook', 'twitter', 'logo', '1px', 'star', 'rating', 'email', 'footer', 'ytimg', 'outil', 'tool', 'ad.', 'adserv', 'pinterest', 'google', 'encart', 'reddit'),
 
-    'extractors' => array(
-        '.*marmiton.*' => '\Extractors\Marmiton',
-        '.*cuisineaz.*' => '\Extractors\CuisineAZ',
-        '.*750g.*' => '\Extractors\g750',
-        '.*odelices.*' => '\Extractors\Odelices',
-        '.*foodnetwork.*' => '\Extractors\FoodNetwork',
-        '.*food.*' => '\Extractors\Food',
-        '.*1001cocktails.*' => '\Extractors\Cocktail1001',
-        '.*lacuisinedannie.*' => '\Extractors\LaCuisineDAnnie',
-        '.*lefigaro.*' => '\Extractors\MadameLeFigaro',
-        '.*allrecipes.*' => '\Extractors\AllRecipes',
-        '.*femina.*' => '\Extractors\Femina',
-        '.*jamieoliver.*' => '\Extractors\JamieOliver',
-        '.*journaldesfemmes.*' => '\Extractors\JournalDesFemmes',
-    ),
+	'extractors' => array(
+		'.*marmiton.*'             => '\Extractors\Marmiton',
+		'.*cuisineaz.*'            => '\Extractors\CuisineAZ',
+		'.*750g.*'                 => '\Extractors\g750',
+		'.*odelices.*'             => '\Extractors\Odelices',
+		'.*foodnetwork.*'          => '\Extractors\FoodNetwork',
+		'.*food.*'                 => '\Extractors\Food',
+		'.*1001cocktails.*'        => '\Extractors\Cocktail1001',
+		'.*lacuisinedannie.*'      => '\Extractors\LaCuisineDAnnie',
+		'.*lefigaro.*'             => '\Extractors\MadameLeFigaro',
+		'.*allrecipes.*'           => '\Extractors\AllRecipes',
+		'.*femina.*'               => '\Extractors\Femina',
+		'.*jamieoliver.*'          => '\Extractors\JamieOliver',
+		'.*journaldesfemmes.*'     => '\Extractors\JournalDesFemmes',
+		'.*cuisine.notrefamille.*' => '\Extractors\CuisineNotreFamille',
+	),
 
-    'defaultExtractor' => '\Extractors\ReadabilityExtractor'
+	'defaultExtractor' => '\Extractors\ReadabilityExtractor'
 );
---
+++ app/libraries/Extractors/CuisineNotreFamille.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Extractors;
+
+class CuisineNotreFamille extends AbstractExtractor {
+
+	public function getTitleCssSelector() {
+		return 'span[id=ctl00_Main_LabelRecetteNom]';
+	}
+
+	public function getYieldCssSelector() {
+		return 'span[id=ctl00_Main_Lab_nb_convives] > .yield';
+	}
+
+	public function getIngredientsCssSelector() {
+		return 'div[id=structure-page-recette] > div.item > ul > li';
+	}
+
+	public function getPreparationsCssSelector() {
+		return '.txt-preparation';
+	}
+}
